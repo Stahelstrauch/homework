6f876127008336c88ad2bfea5bd1eccfba0b8ef8
Stahelstrauch <estahelstrauchpulk@gmail.com>
2025-04-25 14:47:54 +0300

    Delete include/settings.php

--- include/settings.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("DB_SERVER", "localhost"); // Serveri aadress 127.0.0.1
-define("DB_USER", "root"); // Kasutajanimi serverisse logides
-define("DB_PASS", ""); // Parool serverisse logides
-define("DB_NAME", "feedback_system"); //Andmebaasi nimi
