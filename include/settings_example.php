06615fb98c7ad6ae8b5cd144a57cd6d52478edd9
Stahelstrauch <estahelstrauchpulk@gmail.com>
2025-04-25 14:45:10 +0300

    ..k.

---
+++ include/settings_example.php
@@ -0,0 +1,5 @@
+<?php
+define("DB_SERVER", "SERVER NAME"); // Serveri aadress 127.0.0.1
+define("DB_USER", "USERNAME"); // Kasutajanimi serverisse logides
+define("DB_PASS", "PASSWORD"); // Parool serverisse logides
+define("DB_NAME", "DATABASE NAME"); //Andmebaasi nimi
